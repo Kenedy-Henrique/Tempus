4f6440ffa7517b014ae0839fce5a71829cc0a3da
Kenedy-Henrique <kenedyhenrique.buenosilva@gmail.com>
2020-09-30 19:04:56 -0300

    navBar module

--- src/navbar.js
+++ src/navbar.js
@@ -1,13 +1,20 @@
-function navBar(contentDiv) {
-    let introDiv = document.createElement('div');
+function makeNavBar(contentDiv) {
+    let navBar = document.createElement('div');
     let intro = document.createElement('button');
     let menu = document.createElement('button');
     let about = document.createElement('button');
 
-    introDiv.style.display = 'flex';
-    introDiv.style.gap = '3vw';
-    introDiv.style.justifyContent = 'center';
+    navBar.style.display = 'flex';
+    navBar.style.gap = '3vw';
+    navBar.style.justifyContent = 'center';
+    navBar.style.position = 'relative';
+    navBar.style.top = '1vw';
+    navBar.style.backgroundColor = 'white';
+    navBar.style.width = '80vw';
+    navBar.style.left = '50%';
+    navBar.style.transform = 'translateX(-50%)';
 
+    intro.classList.add('introBtn');
     intro.innerHTML = 'Intro';
     intro.style.fontFamily = 'Quicksand';
     intro.style.fontSize = '3vw';
@@ -17,6 +24,7 @@ function navBar(contentDiv) {
     intro.addEventListener("mouseover", () => intro.style.color = 'rgb(125, 35, 60)');
     intro.addEventListener("mouseout", () => intro.style.color = 'rgb(207, 154, 41)');
 
+    menu.classList.add('menuBtn');
     menu.innerHTML = 'Menu';
     menu.style.fontFamily = 'Quicksand';
     menu.style.fontSize = '3vw';
@@ -26,6 +34,7 @@ function navBar(contentDiv) {
     menu.addEventListener("mouseover", () => menu.style.color = 'rgb(125, 35, 60)');
     menu.addEventListener("mouseout", () => menu.style.color = 'rgb(207, 154, 41)');
 
+    about.classList.add('aboutBtn');
     about.innerHTML = 'About';
     about.style.fontFamily = 'Quicksand';
     about.style.fontSize = '3vw';
@@ -35,10 +44,10 @@ function navBar(contentDiv) {
     about.addEventListener("mouseover", () => about.style.color = 'rgb(125, 35, 60)');
     about.addEventListener("mouseout", () => about.style.color = 'rgb(207, 154, 41)');
     
-    introDiv.appendChild(intro);
-    introDiv.appendChild(menu);
-    introDiv.appendChild(about);
-    contentDiv.appendChild(introDiv);
+    navBar.appendChild(intro);
+    navBar.appendChild(menu);
+    navBar.appendChild(about);
+    contentDiv.appendChild(navBar);
 }
 
-export default navBar;
+export default makeNavBar;

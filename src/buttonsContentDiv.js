1b609498cb265354ae993925744cde07591b7440
Kenedy-Henrique <kenedyhenrique.buenosilva@gmail.com>
2020-09-30 19:06:19 -0300

    buttonsContentDiv module first commit - add

---
+++ src/buttonsContentDiv.js
@@ -0,0 +1,7 @@
+function buttonsContentDiv(){
+    let buttonsContentDiv = document.createElement('div');
+    buttonsContentDiv.classList.add('buttonsContentDiv');
+    buttonsContentDiv.style
+}
+
+export default buttonsContentDiv;

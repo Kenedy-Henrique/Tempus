cb2b5d4d2578f9089acf712b15e96b78518b0da0
Kenedy-Henrique <kenedyhenrique.buenosilva@gmail.com>
2020-10-01 13:42:22 -0300

    animationsLineMechanics Module

---
+++ src/animationLineMechanics.js
@@ -0,0 +1,72 @@
+function animationLineMechanics(pastState, presentState){
+    let animationLine = document.querySelector('.animationLine');
+    let introPostion = '7.65vw';
+    let menuPostion = '19vw';
+    let aboutPostion = '31.3vw';
+
+    if(pastState === 'introDiv'){
+        if(presentState === 'menuDiv'){
+            animationLine.animate([
+                { left: introPostion},
+                { left: menuPostion}
+              ], {
+                duration: 200,
+                fill: "forwards"
+            })
+        } else if(presentState === 'aboutDiv'){
+            animationLine.animate([
+                { left: introPostion},
+                { left: aboutPostion}
+              ], {
+                duration: 200,
+                fill: "forwards"
+            })
+        }
+    }
+
+
+
+    if(pastState === 'menuDiv'){
+        if(presentState === 'introDiv'){
+            animationLine.animate([
+                { left: menuPostion},
+                { left: introPostion}
+              ], {
+                duration: 200,
+                fill: "forwards"
+            })
+        } else if(presentState === 'aboutDiv'){
+            animationLine.animate([
+                { left: menuPostion},
+                { left: aboutPostion}
+              ], {
+                duration: 200,
+                fill: "forwards"
+            })
+        }
+    }
+
+
+
+    if(pastState === 'aboutDiv'){
+        if(presentState === 'menuDiv'){
+            animationLine.animate([
+                { left: aboutPostion},
+                { left: menuPostion}
+              ], {
+                duration: 200,
+                fill: "forwards"
+            })
+        } else if(presentState === 'introDiv'){
+            animationLine.animate([
+                { left: aboutPostion},
+                { left: introPostion}
+              ], {
+                duration: 200,
+                fill: "forwards"
+            })
+        }
+    }
+}
+
+export default animationLineMechanics;

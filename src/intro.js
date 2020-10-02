83cb169cae4c9401a3f3da8edec611779caf573b
Kenedy-Henrique <kenedyhenrique.buenosilva@gmail.com>
2020-10-02 13:55:59 -0300

    image add and text change

--- src/intro.js
+++ src/intro.js
@@ -1,23 +1,33 @@
 function insertIntro(contentDiv){
     let introDiv = document.createElement('div');
-    introDiv.classList.add('introDiv');
-    let introDivText = document.createElement('p');
+    let wineBottlesImage = document.createElement('img');
+    let text = document.createElement('p');
 
+    introDiv.classList.add('introDiv');
     introDiv.style.position = 'relative';
     introDiv.style.top = '4.5vw';
     introDiv.style.backgroundColor = 'white';
     introDiv.style.width = '80vw';
-    introDiv.style.height = '150vw';
     introDiv.style.left = '50%';
     introDiv.style.transform = 'translateX(-50%)';
 
-    introDivText.style.fontFamily = 'Quicksand';
-    introDivText.style.fontSize = '2vw';
-    introDivText.style.padding = '2vw';
-    introDivText.style.textAlign = 'center';
-    introDivText.innerHTML = 'The best place if you are looking for the best of the best wine.';
+    wineBottlesImage.src = '../src/assets/wine-bottles_burned.png';
+    wineBottlesImage.style.height = '50vw';
+    wineBottlesImage.style.width = 'auto';
 
-    introDiv.appendChild(introDivText);
+    text.style.fontFamily = 'Playfair Display';
+    text.style.fontSize = '2vw';
+    text.style.padding = '2vw';
+    text.style.position = 'absolute';
+    text.style.top = '10vw';
+    text.style.left = '30vw';
+    text.style.boxShadow = '12px 12px 2px 1px rgba(0, 0, 255, .2)';
+    text.style.color = 'rgba(0, 0, 0, .7)';
+    text.innerHTML = 'The best place if you are looking for the best of the best wine.<br><br>With more than 10 years serving our clients with the best product, we are sure to serve you as well.<br><br>Join that story with us.';
+
+
+    introDiv.appendChild(wineBottlesImage);
+    introDiv.appendChild(text);
     contentDiv.appendChild(introDiv);
 
     return introDiv;
